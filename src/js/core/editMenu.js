92967873e24e4b3bb1106ac757c34813ea5edb61
UselessNaughtyCat <PichuginN1999@yandex.ru>
2018-02-21 22:40:53 +0500

    block highlight, click event dont work

--- src/js/core/editMenu.js
+++ src/js/core/editMenu.js
@@ -2,31 +2,25 @@ import MenuItem from "./menuItem";
 import icon from '../../img/icon-34.png';
 
 const editMenu = {
-  menuItems: [
-      new MenuItem({
-          icon: icon,
-          action: function() {
-              this.style.backgroundColor = 'red';
-          },
-      })
-  ],
-  
-  init: function() {
-    document.body.addEventListener("click", function (e) {
-        alert(e.target.id)
-    })
-    /*
-    this.elem = document.createElement('div');
-    this.elem.classList.add('edit-menu');
-    for (let menuItem of this.menuItems) {
-     this.elem.appendChild(menuItem.elem);   
+    init: function(currentBlock) {
+        this.menuItems = [
+          new MenuItem({
+              icon: icon,
+              action: function() {
+                  console.log(currentBlock);
+              },
+          })
+        ],
+        this.elem = document.createElement('div');
+        this.elem.classList.add('edit-menu');
+        for (let menuItem of this.menuItems) {
+            this.elem.appendChild(menuItem.elem);   
+        }
+        document.body.appendChild(this.elem);
+    },
+    deinit: function() {
+        document.body.removeChild(this.elem);
     }
-    document.body.appendChild(this.elem);
-    */
-  },
-  deinit: function() {
-      document.body.removeChild(this.elem);
-  }
 };
 
 export default editMenu;
